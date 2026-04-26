b5abc444efee8c14add4f9c41a560ba3d9f17ee7
Jaco du Plessis <5507425+NetworkEntropologist@users.noreply.github.com>
2026-04-26 14:31:02 +0200

    Updated to dual license model

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # 28c-eeprom-programmer
 An EEPROM programmer for the 28Cx series of EEPROMs.
+
+## License
+
+This project uses a dual-licensing approach:
+
+- **Hardware** (schematics, PCB layouts, and design files in the `hardware/` directory): Licensed under the [CERN Open Hardware License v2 - Strongly Reciprocal (CERN-OHL-S)](./LICENSE-CERN-OHL)
+- **Software** (firmware, Python control scripts, and other code): Licensed under the [MIT License](./LICENSE-MIT)
+
+See the respective license files for full details.
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,3 +1,15 @@
+/**
+ * @file main.cpp
+ * @brief Arduino Nano firmware for 28Cx EEPROM programmer
+ * @author The Network Entropologist
+ * @version 0.1
+ * @details Supports programming of 28C16, 28C64, 28C256, and 28C512 EEPROMs.
+ *          Hardware: Arduino Nano with 74HC595 shift registers.
+ *          Driven by Python control script.
+ *          Inspired by https://github.com/beneater/eeprom-programmer
+ * @license MIT License (See LICENSE-MIT file in project root)
+ */
+
 #include <Arduino.h>
 
 void setup() {
