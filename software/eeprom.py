dc63eb111d1a38ee926d1b12afca1ab77bc94368
Jaco du Plessis <5507425+NetworkEntropologist@users.noreply.github.com>
2026-07-02 08:31:54 +0200

    Writing files works

--- software/eeprom.py
+++ software/eeprom.py
@@ -8,6 +8,9 @@ from enum import Enum
 
 from serial import Serial
 from serial.tools import list_ports
+from alive_progress import alive_bar
+
+from intelhex import IntelHex
 
 port = Serial()
 port.baudrate = 115200
@@ -35,6 +38,10 @@ command = ''
 addr = 0
 data = 0
 cmd_bytes = b''
+file_mode = False
+max_address = 0
+
+file_contents = {}
 
 def get_port() -> str:
     ports = list_ports.comports()
@@ -144,6 +151,17 @@ def get_data() -> int:
         except ValueError:
             print(f'Invalid selection. Please enter a valid hex data byte.')
 
+def set_max_address(eeprom_type: EEPROMType) -> int:
+    
+    if eeprom_type.value == '28C256':
+        max_address = 0x7FFF
+    elif eeprom_type.value == '28C64':
+        max_address = 0x1FFF
+    elif eeprom_type.value == '28C16':
+        max_address = 0x7FF
+
+    return max_address
+
 def valid_port(port_str: str) -> bool:
     ports_list = [port.device for port in list_ports.comports()]
     if port_str not in ports_list:
@@ -187,6 +205,30 @@ def valid_address(eeprom_type: EEPROMType, address: str) -> bool:
     
     return True
 
+def valid_file(file_path: str) -> bool:
+    global max_address, file_contents
+
+    try:
+        ih = IntelHex()
+        ih.fromfile(file_path, format='hex')
+
+        # Check the file size against the EEPROM type size limit
+        if max_address == 0:
+            print('Cannot validate hex file if EEPROM type is not set.')
+            return False
+
+        file_contents = ih.todict()
+        
+        if len(file_contents) > max_address + 1:
+            print(f'Hex file size ({len(file_contents)} bytes) exceeds the maximum addressable space of the selected EEPROM type ({max_address + 1} bytes). Switching to interactive mode.')
+            return False
+        
+        return True
+    
+    except Exception as e:
+        print(f'Error reading file: {e}.')
+        return False
+
 def valid_data(data: str) -> bool:
     try:
         data_byte = int(data, 16)
@@ -216,8 +258,141 @@ def build_write_command(address: int, data: int) -> bytes:
     cmd_byte = 'W'.encode('utf-8')[0]
     return pack(format_string, 0x01, 0x04, cmd_byte, address, data)
 
+def format_hex_output(hex_dict: dict):
+
+    current_address = 0
+    formatted_output = ''
+
+    total_bytes = len(hex_dict)
+    lines_needed = (total_bytes + 15) // 16
+
+    for line in range(lines_needed):
+        line_bytes = []
+        for i in range(16):
+            byte_value = hex_dict.get(current_address + i, 0xFF)  # Default to 0xFF if address is not in the dict
+            line_bytes.append(byte_value)
+        
+        # Format the line output
+        line_output = f"{current_address:04X} " + " "
+        for i in range (8):
+            line_output += f"{line_bytes[i]:02X} "
+        line_output += " "  # Extra space between the two groups of 8 bytes
+        for i in range(8, 16):
+            line_output += f"{line_bytes[i]:02X} "
+        
+        # And finally add the ASCII representation of the bytes at the end of the line
+        ascii_representation = ''.join(chr(b) if 32 <= b <= 126 else '.' for b in line_bytes)
+        line_output += " " + ascii_representation
+        formatted_output += line_output + '\n'
+
+        current_address += 16
+
+    return formatted_output
+
+def do_read(port: Serial, address: int) -> bool:
+    """
+    Send a read command to the Arduino and wait for a response.
+    Parameters:
+        port (Serial): The serial port to use for communication.
+        address (int): The EEPROM address to read from.
+    Returns:
+        bool: True if the command was sent successfully, False otherwise.
+    """
+
+    cmd_bytes = build_read_command(address)
+    print(f'Sending read command: {cmd_bytes.hex()}...')
+    port.write(cmd_bytes)
+    port.flush()  # Ensure data is sent immediately
+
+    time.sleep(0.5)  # Give Arduino time to process the command
+
+    while not port.in_waiting:
+        pass
+
+    return True
+
+def do_write(port: Serial, address: int, data: int) -> bool:
+    """
+    Send a write command to the Arduino and wait for a response.
+    Parameters:
+        port (Serial): The serial port to use for communication.
+        address (int): The EEPROM address to write to.
+        data (int): The data byte to write to the specified address.
+    Returns:
+        bool: True if the command was sent successfully, False otherwise.
+    """
+    cmd_bytes = build_write_command(address, data)
+    # print(f'Sending write command: {cmd_bytes.hex()}...')
+    port.write(cmd_bytes)
+    port.flush()  # Ensure data is sent immediately
+
+    time.sleep(0.5)  # Give Arduino time to process the command
+
+    while not port.in_waiting:
+        pass
+
+    return True
+
+def process_write(port: Serial, mode: str, address: int = 0, data: int = 0, file_contents: dict = {}) -> bool:
+
+    print('Opening serial port...')
+    port.open()
+    time.sleep(2)  # Give the Arduino time to initialize after opening the port
+    print('Serial port opened.')
+
+    if mode == 'file':
+        with alive_bar(len(file_contents), title='Writing to EEPROM', bar='blocks', stats=False, monitor=False) as bar:
+            for addr, byte in file_contents.items():
+                bar.text(f'Writing byte 0x{byte:02X} to address 0x{addr:04X}')
+                bar()
+                #time.sleep(0.1)
+                if not do_write(port, addr, byte):
+                    port.close()
+                    print('Serial port closed.')
+                    return False
+                if port.in_waiting > 0:
+                    response = port.read(port.in_waiting)
+                    print(f'Response: {response.hex()}')
+                    bar.text(f'Byte 0x{byte:02X} written to address 0x{addr:04X}. Response: {response.hex()}')
+                    bar()
+                else:
+                    bar.text(f'Byte 0x{byte:02X} written to address 0x{addr:04X}. No response received (timeout)')
+                    bar()
+                    port.close()
+                    print('Serial port closed.')
+                    return False
+                
+        print(f'File write complete. {len(file_contents)} bytes written.')
+    else:
+        if not do_write(port, address, data):
+            port.close()
+            print('Serial port closed.')
+            return False
+        if port.in_waiting > 0:
+            response = port.read(port.in_waiting)
+            print(f'Response: {response.hex()}')
+        else:
+            print('No response received (timeout)')
+            port.close()
+            print('Serial port closed.')
+            return False
+
+    port.close()
+    print('Serial port closed.')
+    return True
+
+def process_command(addr: int, data: int) -> bool:
+    match command:
+        case 'READ' | 'R':
+            return do_read(port, addr)
+        case 'WRITE' | 'W':
+            return process_write(port, 'file' if file_mode else 'single', addr, data, file_contents)
+        case _:
+            print(f'Invalid command: {command}. Exiting.')
+            sys.exit(1)
+
 def parse_args(args):
-    global command, addr, data, eeprom_type
+    global command, addr, data, eeprom_type, file_mode, file_contents, max_address
 
     # No args specified so switch to interactive mode
     if len(sys.argv) == 1:
@@ -240,6 +415,7 @@ def parse_args(args):
     else:
         eeprom_type = EEPROMType(dev_type)
         print(f'Using specified EEPROM type: {eeprom_type.value}')        
+    max_address = set_max_address(eeprom_type)
 
     cmd = args.command.upper() if args.command else ''
     print(f'Command specified: {cmd}')
@@ -250,23 +426,35 @@ def parse_args(args):
         print(f'Using specified command: {cmd}')
         command = cmd
 
-    addr = args.address
-    if not addr or not valid_address(eeprom_type, addr):
-        print('No valid address specified. Switching to interactive mode.')
-        addr = get_address()
-    else:
-        addr = int(addr, 16)
-        print(f'Using specified address: {hex(addr)}')
 
-    if command == 'W':
+    addr = args.address
+    if command == 'R' or command == 'READ' or ((command == 'W' or command == 'WRITE') and not args.file):
+        if not addr or not valid_address(eeprom_type, addr):
+            print('No valid address specified. Switching to interactive mode.')
+            addr = get_address()
+        else:
+            addr = int(addr, 16)
+            print(f'Using specified address: {hex(addr)}')
+
+    if command == 'W' or command == 'WRITE':
         data_arg = args.data
-        if not data_arg or not valid_data(data_arg):
+        file_path_arg = args.file
+        # if not data_arg or not file_path_arg or not valid_data(data_arg):
+        if not data_arg and not file_path_arg:
             print('No valid data specified. Switching to interactive mode.')
             data = get_data()
         else:
-            data = int(data_arg, 16)
-            print(f'Using specified data: {hex(data)}')
-    
+            print(f'File path specified: {file_path_arg}')
+            if not valid_file(file_path_arg):
+                print('Invalid file specified. Switching to interactive mode.')
+                file_mode = False
+            else:
+                file_mode = True
+                print(f'Using specified file: {file_path_arg}')
+                confirm_input = input('View file contents? [y]es/[N]o: ')
+                if confirm_input.upper() == 'Y':
+                    print(format_hex_output(file_contents))
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='EEPROM Programmer')
@@ -291,39 +479,41 @@ if __name__ == '__main__':
         print('User aborted. Exiting.')
         sys.exit(0)
 
-    print(f'Command: {command}, Address: {hex(addr)}, Data: {hex(data) if command == "W" else "N/A"}')
+    # print(f'Command: {command}, Address: {hex(addr)}, Data: {hex(data) if command == "W" else "N/A"}')
 
-    match command:
-        case 'READ' | 'R':
-            cmd_bytes = build_read_command(addr)
-        case 'WRITE' | 'W':
-            cmd_bytes = build_write_command(addr, data)
-        case _:
-            print(f'Invalid command: {command}. Exiting.')
-            sys.exit(1)
+    process_command(addr, data)
 
-    print('Opening serial port...')
-    port.open()
-    time.sleep(2)  # Give the Arduino time to initialize after opening the port
-    print('Serial port opened.')
+    # match command:
+    #     case 'READ' | 'R':
+    #         cmd_bytes = build_read_command(addr)
+    #     case 'WRITE' | 'W':
+    #         cmd_bytes = build_write_command(addr, data)
+    #     case _:
+    #         print(f'Invalid command: {command}. Exiting.')
+    #         sys.exit(1)
 
-    print(f'Sending command: {cmd_bytes.hex()}...')    
-    port.write(cmd_bytes)
-    port.flush()  # Ensure data is sent immediately
+    # print('Opening serial port...')
+    # port.open()
+    # time.sleep(2)  # Give the Arduino time to initialize after opening the port
+    # print('Serial port opened.')
 
-    time.sleep(0.5)  # Give Arduino time to process the command
+    # print(f'Sending command: {cmd_bytes.hex()}...')    
+    # port.write(cmd_bytes)
+    # port.flush()  # Ensure data is sent immediately
 
-    while not port.in_waiting:
-        pass
+    # time.sleep(0.5)  # Give Arduino time to process the command
 
-    if port.in_waiting > 0:
-        print('Response received')
-        response = port.read(port.in_waiting)
-        # Print the raw response
-        print(f'Response: {response.hex()}')
-        # Now print the response in a more human-readable format
-    else:
-        print('No response received (timeout)')
+    # while not port.in_waiting:
+    #     pass
+
+    # if port.in_waiting > 0:
+    #     print('Response received')
+    #     response = port.read(port.in_waiting)
+    #     # Print the raw response
+    #     print(f'Response: {response.hex()}')
+    #     # Now print the response in a more human-readable format
+    # else:
+    #     print('No response received (timeout)')
 
 
 
